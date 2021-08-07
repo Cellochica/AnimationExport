c2fbb21df7b091fea238eb6aaea2987830ce2d5f
Eli <smitel06@hotmail.com>
2021-08-08 02:53:25 +0930

    walk setup done

---
+++ Assets/AnimationBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationBehaviour : MonoBehaviour
+{
+
+    Animator m_Animator;
+    // Start is called before the first frame update
+
+    //create controls to control demon model
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>(); //set animator
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if button pressed set trigger
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.RightArrow))
+        {
+            //set to walk
+            m_Animator.SetTrigger("walk");
+        }
+        else
+        {
+            m_Animator.SetTrigger("idle");
+        }
+
+    }
+}
